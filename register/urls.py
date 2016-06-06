76aeeba3ee1adf38b108f00e99395f5a95287d1f
appavus <ananthakrishnanh97@gmail.com>
2016-06-06 12:41:01 +0530

    chocolate

--- register/urls.py
+++ register/urls.py
@@ -9,6 +9,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', UserRegistrationView.as_view(), name='register_user'),
     url(r'^user/success/', TemplateView.as_view(template_name='sucess.html'),
-        name='page')
+        name='page'),
+    url( r'^chocolate/add/', AddChocolateView.as_view(), name="add_chocolate")
 
 ]
--- register/views.py
+++ register/views.py
@@ -4,6 +4,7 @@ from braces.views import LoginRequiredMixin, AnonymousRequiredMixin
 from django.views.generic.edit import FormView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from forms import UserRegistrationForm
+from forms import ChocolateAddForm
 
 # Create your views here.
 class Home(TemplateView):
@@ -14,6 +15,14 @@ class UserRegistrationView(AnonymousRequiredMixin, FormView):
     template_name = "register_user.html"
     authenticated_redirect_url = reverse_lazy(u"home")
     form_class = UserRegistrationForm
+    success_url = '/register/user/success'
+
+    def form_valid(self, form):
+        form.save()
+        return FormView.form_valid(self, form)
+class AddChocolateView(FormView):
+    template_name = "add_chocolate.html"
+    form_class = ChocolateAddForm
     success_url = '/register/user/success'
 
     def form_valid(self, form):
